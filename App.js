606ea0cd45ab5906111fef5081a470d9098d8b0f
Shubham Singh <s.singh@agilecockpit.com>
2024-02-13 10:57:04 +0530

    changes

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { Provider } from 'react-redux';
 import Login from './modules/login/components/screen/Login';
+import store from './modules/store/rootReducer.js';
 
 export default function App() {
   return (
-      <Login/>
+    <Provider store={store}>
+      <Login />
+    </Provider>
+
   )
 }
